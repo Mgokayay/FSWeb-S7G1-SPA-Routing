359a1f4e6003ac78cdd2069389081ca71f9c1c0f
Mgokayay <aymustafagokay@gmail.com>
2023-10-31 16:50:28 +0300

    proje yapıldı

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {NavLink,Link} from "react-router-dom"
 
 export default function KaydedilenlerListesi(props) {
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+       <NavLink to={`/filmler/${movie.id}`} className={(isActive)=> isActive? "active" : null}> <span className="saved-movie">{movie.title}</span></NavLink>
       ))}
+      <Link to="/">
       <div className="home-button">Anasayfa</div>
+      </Link>
     </div>
+    
   );
 }
